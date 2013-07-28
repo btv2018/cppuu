9a41f9649f953cab7a050d3d08812748a0781514
btv2018 <btv@heavyside.ru>
2013-07-28 19:50:48 +0400

    added implemenation with boost::any

--- include/json_generator.h
+++ include/json_generator.h
@@ -1,8 +1,97 @@
 #include <string>
+#include <vector>
 #include <ostream>
 #include <iostream>
 
+#include <boost/any.hpp>
+
 namespace cppuu { namespace json {
+
+struct ja;
+struct jo;
+
+void write_a(std::ostream& os, const ja* a);
+void write_o(std::ostream& os, const jo* o);
+
+struct jv {
+    enum ValueType {
+        NIL,
+        BOOL,
+        INT,
+        STRING,
+        ARRAY,
+        OBJECT,
+    };
+    ValueType type;
+
+    boost::any value;
+    jv() : type(NIL) {}
+    jv(int v) : value(v), type(INT) {}
+    jv(bool v) : value(v), type(BOOL) {}
+    jv(const std::string& v) : value(v), type(STRING) {}
+    jv(const ja& v) : value(v), type(ARRAY) {}
+    jv(const jo& v) : value(v), type(OBJECT) {}
+    void write(std::ostream& os) const {
+        if (type == NIL) {
+            os << "null";
+        } else if (type == BOOL) {
+            bool v = boost::any_cast<bool>(value);
+            os << (true ? "true" : "false");
+        } else if (type == INT) {
+            os << boost::any_cast<int>(value);
+        } else if (type == STRING) {
+            os << *boost::any_cast<std::string>(&value);
+        } else if (type == ARRAY) {
+            write_a(os, boost::any_cast<ja>(&value));
+        } else if (type == OBJECT) {
+            write_o(os, boost::any_cast<jo>(&value));
+        }
+    }
+};
+
+struct ja {
+    std::vector<jv> jvs;
+    void write(std::ostream& os) const {
+        os << '[';
+        for (auto it = begin(jvs); it != end(jvs); ++it) {
+            os << ", ";
+            it->write(os);
+        }
+        os << ']';
+    }
+};
+
+struct jp {
+    std::string key;
+    jv value;
+    jp() {}
+    jp(const std::string& key, const jv& value) : key(key), value(value) {}
+    void write(std::ostream& os) const {
+        os << key << ": ";
+        value.write(os);
+    }
+};
+
+struct jo {
+    std::vector<jp> jps;
+    void write(std::ostream& os) const {
+        os << '{';
+        for (auto it = begin(jps); it != end(jps); ++it) {
+            os << ", ";
+            it->write(os);
+        }
+        os << '}';
+    }
+};
+
+
+void write_a(std::ostream& os, const ja* a) {
+    a->write(os);
+}
+
+void write_o(std::ostream& os, const jo* o) {
+    o->write(os);
+}
 
 std::string quote_json_string(const char* str) {
     std::string result("\"");
--- tests/test_json_generator.cpp
+++ tests/test_json_generator.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <boost/any.hpp>
 
 #include "json_generator.h"
 
@@ -14,29 +15,59 @@ using namespace cppuu::json;
 
 int main() {
     
-    json_string obj("key", "value");
-    obj.write(std::cerr);
+//    json_string obj("key", "value");
+//    obj.write(std::cerr);
+//    std::cerr << std::endl;
+//
+//    json_t<int> obj_i = make("key", 3);
+//    //json_t<int> obj_i("key", 3);
+//    obj_i.write(std::cerr);
+//    std::cerr << std::endl;
+//
+//    json_t<double> obj_d = make("key", 3.1);
+//    obj_d.write(std::cerr);
+//    std::cerr << std::endl;
+//
+//    json_t<bool> obj_b = make("key", true);
+//    obj_b.write(std::cerr);
+//    std::cerr << std::endl;
+//
+//    json_t<const char*> obj_s = make("key", "value");
+//    obj_s.write(std::cerr);
+//    std::cerr << std::endl;
+//
+//    auto obj2 = make("root", make("key", false));
+//    obj2.write(std::cerr);
+//    std::cerr << std::endl;
+
+    //any.
+    jv o1(2);
+    o1.write(std::cerr);
+    std::cerr << std::endl;
+    jv o2(false);
+    o2.write(std::cerr);
     std::cerr << std::endl;
 
-    json_t<int> obj_i = make("key", 3);
-    //json_t<int> obj_i("key", 3);
-    obj_i.write(std::cerr);
+    jp p1("k1", o1);
+    p1.write(std::cerr);
+    std::cerr << std::endl;
+//    p1.key = "k1";
+//    p1.value = o1;
+
+    jo obj1;
+    obj1.jps = std::vector<jp>(1);
+    obj1.write(std::cerr);
     std::cerr << std::endl;
 
-    json_t<double> obj_d = make("key", 3.1);
-    obj_d.write(std::cerr);
-    std::cerr << std::endl;
+    jv v3;
+    v3 = o1;
 
-    json_t<bool> obj_b = make("key", true);
-    obj_b.write(std::cerr);
-    std::cerr << std::endl;
-
-    json_t<const char*> obj_s = make("key", "value");
-    obj_s.write(std::cerr);
-    std::cerr << std::endl;
-
-    auto obj2 = make("root", make("key", false));
-    obj2.write(std::cerr);
+    ja a1;
+    std::vector<jv> jvs;
+    jvs.push_back(o1);
+    jvs.push_back(o2);
+    a1.jvs = jvs;
+    a1.write(std::cerr);
     std::cerr << std::endl;
 
     return 0;
